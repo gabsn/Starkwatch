76287335664ff685f5680460efa91334b8d24b9b
Yoann Tilmont <yoanntilmont@gmail.com>
2022-10-28 23:23:40 +0100

    fetcher lambda

---
+++ cdk/functions/index.ts
@@ -0,0 +1,3 @@
+export const handler = (event: any) => {
+    console.log(event);
+}
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,13 +1,32 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import * as path from 'path'
+import { Duration } from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, "CdkQueue", {
-      visibilityTimeout: cdk.Duration.seconds(300),
+    const newLambda = new NodejsFunction(this, "Fetcher", {
+      timeout: Duration.seconds(40),
+      memorySize: 256,
+      entry: path.resolve(__dirname, `../functions/index.ts`),
+      runtime: Runtime.NODEJS_16_X,
+      logRetention: RetentionDays.ONE_WEEK,
+      bundling: {
+        minify: true,
+        externalModules: ["aws-sdk"],
+      },
     });
-  }
+
+    const eventRule = new events.Rule(this, 'scheduleRule', {
+      schedule: events.Schedule.rate(Duration.minutes(1)),
+    });
+    eventRule.addTarget(new targets.LambdaFunction(newLambda))
+  }  
 }
