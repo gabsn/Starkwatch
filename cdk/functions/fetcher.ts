8c7cf7d9102e592b16dc42b590f5664b44de885b
Yoann Tilmont <yoanntilmont@gmail.com>
2022-10-29 00:41:55 +0100

    Implement the fetcher logic

--- cdk/functions/fetcher.ts
+++ cdk/functions/fetcher.ts
@@ -1,3 +1,4 @@
+import { DynamoDBClient, GetItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import axios from "axios";
 
 const getBaseUrl = ({ to, env }: { to: string; env: string }) => {
@@ -11,5 +12,52 @@ export const handler = async (event: any) => {
       to: "0x367c0c4603a29bc5aca8e07c6a2776d7c0d325945abb4f772f448b345ca4cf7",
     })
   );
-  const items = res.data["items"] as Array<{ hash: string; status: string }>;
+  const items = res.data["items"] as Array<Item>;
+  for (const item of items) {
+    // if (item.status === "Accepted on L1") {
+    //   continue;
+    // }
+    const previousItem = await getPreviousItem(item.hash);
+    if (previousItem === null || previousItem.status !== item.status) {
+      emitEventHasChanged();
+      setTransactionStatus(item);
+    }
+  }
 };
+
+async function getPreviousItem(hash: string): Promise<Item | null> {
+  const ddb = new DynamoDBClient({});
+  const params = {
+    TableName: "CdkStack-TransactionStatus2B6158A7-17RI80QLXDL7O",
+    Key: { transactionHash: { S: hash } },
+  };
+  
+  const res = await ddb.send(new GetItemCommand(params));
+  console.log(res);
+
+  return null
+} 
+
+
+function emitEventHasChanged() {
+  return null
+} 
+
+async function setTransactionStatus(item: Item) {
+  const ddb = new DynamoDBClient({});
+  const params = {
+    TableName: "CdkStack-TransactionStatus2B6158A7-17RI80QLXDL7O",
+    Item: { transactionHash: { S: item.hash }, transactionStatus: { S: item.status } },
+  };
+  
+  const res = await ddb.send(new PutItemCommand(params));
+  console.log(res);
+
+  return null
+} 
+
+interface Item {
+  hash: string;
+  status: string;
+  
+}
